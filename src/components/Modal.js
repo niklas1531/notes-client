c9b8f4942c5438af5dc980006215972e91291249
Niklas Minth <niklas.minth@gmail.com>
2023-01-31 22:09:54 +0100

    category fct added

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { useCookies } from "react-cookie";
 
-const Modal = ({ mode, changeShowModal, note, getData , setShowLoader}) => {
+const Modal = ({ mode, changeShowModal, note, getData, setShowLoader }) => {
     const [cookies, setCookie, removeCookie] = useCookies(null)
     const [data, setData] = useState({
         user_email: mode === 'Edit' ? note.user_email : cookies.Email,
         title: mode === 'Edit' ? note.title : '',
         progress: mode === 'Edit' ? note.progress : '2',
         date: new Date().toLocaleString(),
+        category: mode === 'Edit' ? note.category : 'red'
     })
 
     const handleChange = (e) => {
@@ -50,7 +51,7 @@ const Modal = ({ mode, changeShowModal, note, getData , setShowLoader}) => {
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(data)
             })
-            if(response.status === 200){
+            if (response.status === 200) {
                 changeShowModal(false)
                 console.log('Edited!')
                 setShowLoader(false)
@@ -69,10 +70,14 @@ const Modal = ({ mode, changeShowModal, note, getData , setShowLoader}) => {
                     <h3>{mode} task</h3>
                     <button className='close' onClick={() => changeShowModal(false)}>X</button>
                 </div>
-                <form onSubmit={mode=== 'Create' ? postData : editData}>
-                    <input type="text" placeholder='title' maxLength='300' onChange={handleChange} name='title' value={data.title} required/>
+                <form onSubmit={mode === 'Create' ? postData : editData}>
+                    <input type="text" placeholder='title' maxLength='300' onChange={handleChange} name='title' value={data.title} required />
                     <label htmlFor="range">Drag to select your current progress:</label>
                     <input id='range' type="range" min={'2'} max={'100'} onChange={handleChange} name='progress' value={data.progress} />
+                    <div className="flex">
+                        <label htmlFor="category">Choose a Category</label>
+                        <input type="color" name="category" id="category" onChange={handleChange} value={data.category} />
+                    </div>
                     <input type="submit" className='submit' value={mode} />
                 </form>
             </div>
--- src/components/Note.js
+++ src/components/Note.js
@@ -12,7 +12,7 @@ const Note = ({ note, getData, setShowLoader }) => {
             const response = await fetch(`https://notes-server-production-9f36.up.railway.app/notes/${note.id}`, {
                 method: 'DELETE'
             })
-            if(response.status === 200){
+            if (response.status === 200) {
                 setShowLoader(false)
                 getData()
             }
@@ -23,16 +23,20 @@ const Note = ({ note, getData, setShowLoader }) => {
     }
     return (
         <div className="note">
+
             <div>
-                <h4 className="title">{note.title}</h4>
+                <div className="flex">
+                    <div className="category" style={{ backgroundColor: `${note.category}` }}></div>
+                    <h4 className="title">{note.title}</h4></div>
                 <p className="date">{note.date}</p>
             </div>
+
             <Progress progress={note.progress} />
             <div className="header">
                 <button className="edit" onClick={() => changeShowModal(true)}>EDIT</button>
                 <button className="delete" onClick={deleteNote}>DELETE</button>
             </div>
-            {showModal && <Modal changeShowModal={changeShowModal} mode={'Edit'} getData={getData} note={note} setShowLoader={setShowLoader}/>}
+            {showModal && <Modal changeShowModal={changeShowModal} mode={'Edit'} getData={getData} note={note} setShowLoader={setShowLoader} />}
         </div>
     );
 }
