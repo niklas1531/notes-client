fa406f7332434744c9b5315fd7a75d0d0ababea2
Niklas Minth <niklas.minth@gmail.com>
2023-02-01 11:44:32 +0100

    mobile version btns changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { useCookies } from "react-cookie";
 import Modal from "./Modal";
 import Create from '../images/create.png'
 import Dots from '../images/logout.png'
+import Refresh from '../images/refresh.png'
 const Header = ({getData, setShowLoader}) => {
     const [cookies, setCookie, removeCookie] = useCookies(null)
     const [showModal, changeShowModal] = useState(false)
@@ -14,6 +15,9 @@ const Header = ({getData, setShowLoader}) => {
         window.location.reload()
         setShowLoader(false)
     }
+    const refreshpage = (e) => {
+window.location.reload()
+    }
     return (
         <div className="header">
             <div className="heading">
@@ -21,8 +25,8 @@ const Header = ({getData, setShowLoader}) => {
                 <p>by {Email}</p>
             </div>
             <div className="btn-group">
-                <button className="create" onClick={()=> changeShowModal(true)}> <img src={Create} alt="create" />
-                </button>
+                <button className="create" onClick={()=> changeShowModal(true)}> <img src={Create} alt="create" /></button>
+                <button className="refresh" onClick={refreshpage}> <img src={Refresh} alt="refresh" /></button>
                 <button className='logout' onClick={logout}> <img src={Dots} alt="dots" /> </button>
             </div>
             {showModal && <Modal changeShowModal={changeShowModal} mode={'Create'} getData={getData} setShowLoader={setShowLoader}/>}
